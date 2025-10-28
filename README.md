e1728c0c681cf3908c47a06dde17e3c782a5712b
darkermango <darkermango@proton.me>
2025-10-28 15:51:53 +0300

    Add project files.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Keyium
+Keyium is a blazingly-fast keyboard tactility emulator (KTE) made to improve your typing experience on any keyboard.
+
---
+++ src/Keyium.CLI/Program.cs
@@ -0,0 +1,24 @@
+﻿/**
+* Keyium.CLI, Program.cs.
+* Copyright (c) 2025 DarkerMango. All rights reserved.
+* SPDX-License-Identifier: GPL-3.0-or-later
+*
+* This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+**/
+
+namespace Keyium.CLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
