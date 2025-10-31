24e242cebea4c0df712de7c8baff7b1a04257453
darkermango <darkermango@proton.me>
2025-10-31 10:31:04 +0300

    Implement keyboard Input handling

---
+++ src/Keyium.Core/KeyboardInput/KeyboardHook.cs
@@ -0,0 +1,37 @@
+﻿/**
+* Keyium.Core, KeyboardHook.cs.
+* Copyright (c) 2025 DarkerMango. All rights reserved.
+* SPDX-License-Identifier: GPL-3.0-or-later
+*
+* This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+**/
+
+using SharpHook;
+
+namespace Keyium.Core.KeyboardInput
+{
+    public class KeyboardHook
+    {
+        private readonly TaskPoolGlobalHook _hook;
+
+        public KeyboardHook()
+        {
+            _hook = new();
+        }
+
+        public void StartListening(IKeyboardAdapter listener)
+        {
+            _hook.KeyPressed += listener.KeyPressed;
+            _hook.KeyReleased += listener.KeyReleased;
+            _hook.KeyTyped += listener.KeyTyped;
+
+            _hook.Run();
+        }
+    }
+}
